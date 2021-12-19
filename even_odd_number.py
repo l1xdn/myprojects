dd19640a2be78709ad58daa37a7dec110fe62b9b
l1xdn <81413627+l1xdn@users.noreply.github.com>
2021-12-19 19:09:33 +0300

    Create even_odd_number.py

---
+++ even_odd_number.py
@@ -0,0 +1,9 @@
+print("welcome to luke's projects")
+n = int(input("what number do you want to check?\n"))
+m = n % 2
+if m == 0:
+ print("This number is even!")
+elif m == 1:
+ print("This number is odd!")
+else:
+ print('please put a valid number')
