a33dbb4d92514ea65a0efb6c4dee2a40bbd50710
l1xdn <81413627+l1xdn@users.noreply.github.com>
2021-12-23 17:22:42 +0300

    update

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,10 +1,10 @@
-h = float(input("whats ur hight in m"))
-w = float(input("whats ur wight in kg"))
+h = float(input("whats ur hight in m: "))
+w = float(input("whats ur wight in kg: "))
 bmi = round(w / h **2)
 print(bmi)
 if bmi < 18.5:
- print("you are underweight!")
+ print(f"your bmi is {bmi} you are underweight!")
 elif bmi < 25:
- print("u are in the normal wight")
+ print(f"your bmi is {bmi} you are in the normal wight")
 elif bmi < 30 :
- print("slightly overweight")
+ print(f"your bmi is {bmi} you are slightly overweight")
